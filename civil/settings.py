20ebb81079d65d2ca75537fb4ea09f068f1d12e2
ddkyumaprogrammer <ddkyuma.programmer@gmail.com>
2019-06-19 23:51:00 +0430

    change refresh time to 900sec

--- civil/settings.py
+++ civil/settings.py
@@ -340,7 +340,8 @@ CELERY_TIMEZONE = 'Asia/Tehran'
 CELERY_BEAT_SCHEDULE = {
     'refresh-sms-token-every-30-minutes': {
         'task': 'celery_sandbox.tasks.refresh_sms_token',
-        'schedule': crontab(minute ='*/30')  # refresh every 20 minutes
+        # 'schedule': crontab(minute ='*/30')  # refresh every 20 minutes
+        'schedule': 900.0,  # refresh every 20 minutes
     },
 
 }
