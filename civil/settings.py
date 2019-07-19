a19dd72b1ad280e2fcb29d04c6c5f2fe7d187adf
ddkyumaprogrammer <ddkyuma.programmer@gmail.com>
2019-07-19 15:09:26 +0430

    sel celery-result

--- civil/settings.py
+++ civil/settings.py
@@ -248,7 +248,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # STATICFILES_DIRS = (os.path.join('static'), )
 
 MEDIA_URL='/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT= os.path.join(BASE_DIR,'static/media/')
 # MEDIA_DIRS=(os.path.join('media'), )
 
 
