a1a2b28c2482bf03996ae567a7818fc9f076e399
ddkyumaprogrammer <ddkyuma.programmer@gmail.com>
2019-07-19 15:13:02 +0430

    edit upload image 4

--- meeting/utils.py
+++ meeting/utils.py
@@ -50,7 +50,7 @@ def get_image_path(instance, filename):
     slug = slugify(title)
     basename, file_extension = filename.split(".")
     date = jdatetime.datetime.now().date()
-    new_filename = "%s--%s.%s" % (slug , date , file_extension)
+    new_filename = "%s.%s" % (date , file_extension)
     return "%s/%s" % (slug, new_filename)
 
 
