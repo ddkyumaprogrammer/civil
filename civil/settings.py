36570c2567be0fb70169a103694456424ff95b98
ddkyumaprogrammer <ddkyuma.programmer@gmail.com>
2019-07-19 15:29:37 +0430

    edit upload image 5

--- civil/settings.py
+++ civil/settings.py
@@ -244,13 +244,13 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-# STATICFILES_DIRS = (os.path.join('static'), )
+# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+STATICFILES_DIRS = (os.path.join('static'), )
 
 MEDIA_URL='/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'static/media/')
+MEDIA_ROOT= os.path.join(BASE_DIR,'media/')
 # MEDIA_DIRS=(os.path.join('media'), )
-
+#
 
 
 
