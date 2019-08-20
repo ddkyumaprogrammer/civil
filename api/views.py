7bfd395c696f202d42b7a223768812edb15e5348
DNA.h <s.n.bahador@gmail.com>
2019-08-20 13:43:07 +0430

    -session-by-id: .place_address (3)

--- api/views.py
+++ api/views.py
@@ -520,7 +520,7 @@ def get_session_by_id(request):
         'meeting_owner': str(_session.meeting_owner.first_name) + '-' + str(_session.meeting_owner.last_name),
         'start_time': str(_session.start_time),
         'end_time': str(_session.end_time),
-        'place_address': _session.place,
+        'place_address': _session.place.place_address,
         'people': r
     })
     return JsonResponse(session, safe=False)
