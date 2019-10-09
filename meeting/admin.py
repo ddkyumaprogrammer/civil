fcc5a512dedfca81956c40e09ef0496fe62c4964
DNA.h <s.n.bahador@gmail.com>
2019-10-09 17:56:06 +0330

    -fcm token added (2)

--- meeting/admin.py
+++ meeting/admin.py
@@ -68,7 +68,7 @@ class Peoplesadmin(admin.ModelAdmin):
     list_display = ('_peoples','mobile','is_legal','_places')
     fieldsets =(
                 (None,{
-                     'fields':(('first_name','last_name'),('mobile','is_legal'),('image','_image'))
+                     'fields':(('first_name','last_name'),('mobile','is_legal','fcm_token'),('image','_image'))
                 }),
                 ('بیشتر', {
                             'classes': ('collapse',),
